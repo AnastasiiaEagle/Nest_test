b16f82f02856b4535bd0462b9ad76225ae2833dc
Dellari0n <anastasia.ea9le@gmail.com>
2025-04-30 20:55:32 +0300

    db

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -3,11 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
 
   controllers: [AppController],
   providers: [AppService],
-  imports: [TasksModule, MovieModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 1000,
+    username:'postgres',
+    password: '5432',
+    database: 'nest_test',
+    autoLoadEntities: true,
+    synchronize: true,
+  }), TasksModule, MovieModule],
 })
 export class AppModule {}
--- nest/src/movie/movie.controller.ts
+++ nest/src/movie/movie.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
-import { MovieService } from './movie.service';
+import { Body, Controller, Get, Headers, Param, Post, Query, Req, Res } from '@nestjs/common';
+import { Response } from 'express';
 
-@Controller('movie')
+
+@Controller({
+  path: 'movies',
+  // host: '' // хост з якого буде здійснено запит
+})
 export class MovieController {
-  constructor(private readonly movieService: MovieService) {}
+
+  @Get()
+  findAll(@Query('genre') query: any){ // КВЕРІ ПАРАМЕТРИ
+    return JSON.stringify(query);
+  }
+
+  @Post()
+  create(@Body() body: {title: string; genre: string}){
+    return body;
+  }
+
+  @Get('headers')
+  getHeaders(@Headers() headers: any){
+    return headers;
+  }
+
+  @Get('user')
+  getUserAgent(@Headers('user-agent') userAgent: string){
+    return { userAgent };
+  }
+
+  @Get('req')
+  getRequestDetails(@Req() req: Request){
+    return {
+      method: req.method,
+      url: req.url
+    }
+  }
+
+  @Get('res')
+  getResponseDetails(@Res() res: Response){
+    res.status(201).json({message: 'hello'})
+  }
+
+  @Get(':id')
+  findById(@Param('id') id:string){
+    return {id}
+  }
 }
